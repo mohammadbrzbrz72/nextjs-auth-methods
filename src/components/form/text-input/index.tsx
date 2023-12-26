68b864ceb1e7285e7801d2137fe6e4f2b9a544a8
mohammadbrzbrz72 <mohammadbrzbrz72@gmail.com>
2023-12-26 21:38:03 +0330

    restructure input style

--- src/components/form/text-input/index.tsx
+++ src/components/form/text-input/index.tsx
@@ -1,17 +1,8 @@
 import { useFormContext } from "react-hook-form";
 import clsx from "clsx";
 
-import { ITextInput } from "./types";
-
-const styles = {
-  root: `
-    w-full h-[52px]
-    bg-indigo-50 bg-opacity-80
-    rounded-2xl
-    px-4 py-3
-    focus:outline-gray-300
-  `,
-};
+import type { ITextInput } from "./types";
+import styles from "./styles";
 
 export const TextInput = ({ className, name, placeholder }: ITextInput) => {
   const { register } = useFormContext(); // retrieve all hook methods
---
+++ src/components/form/text-input/styles.ts
@@ -0,0 +1,11 @@
+const styles = {
+  root: `
+    w-full h-[52px]
+    bg-indigo-50 bg-opacity-80
+    rounded-2xl
+    px-4 py-3
+    focus:outline-gray-300
+  `,
+};
+
+export default styles;
